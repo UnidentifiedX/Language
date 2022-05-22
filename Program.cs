d66abf582e6c0cbb93274fbcf89f38ae7a2d6e17
Sun Zizhuo <211530w@student.hci.edu.sg>
2022-05-22 20:05:43 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Language
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
