1659f39dada1cfba5606144f73f49b6f4b0cf5aa
Sun Zizhuo <211530w@student.hci.edu.sg>
2022-05-31 20:42:28 +0800

    Added tests for SourceText

---
+++ Language.Tests/CodeAnalysis/Text/SourceTextTests.cs
@@ -0,0 +1,18 @@
+﻿using Language.CodeAnalysis.Text;
+using Xunit;
+
+namespace Language.Tests.CodeAnalysis.Text
+{
+    public class SourceTextTests
+    {
+        [Theory]
+        [InlineData(".", 1)]
+        [InlineData(".\r\n", 2)]
+        [InlineData(".\r\n\r\n", 3)]
+        public void SourceText_IncludesLastLine(string text, int exprectedLineCount)
+        {
+            var sourceText = SourceText.From(text);
+            Assert.Equal(exprectedLineCount, sourceText.Lines.Length);
+        }
+    }
+}
