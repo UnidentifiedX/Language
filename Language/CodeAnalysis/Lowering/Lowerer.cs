f969ddf60d5127934727d87c0f2d9712e90c2ec9
Sun Zizhuo <211530w@student.hci.edu.sg>
2022-06-05 20:56:05 +0800

    Lowered 'for' statements into 'while' statements

--- Language/CodeAnalysis/Lowering/Lowerer.cs
+++ Language/CodeAnalysis/Lowering/Lowerer.cs
@@ -1,4 +1,5 @@
 ﻿using Language.CodeAnalysis.Binding;
+using System.Collections.Immutable;
 
 namespace Language.CodeAnalysis.Lowering
 {
@@ -13,6 +14,31 @@ namespace Language.CodeAnalysis.Lowering
         {
             var lowerer = new Lowerer();
             return lowerer.RewriteStatement(statement);
+        }
+
+        protected override BoundStatement RewriteForStatement(BoundForStatement node)
+        {
+            var variableDeclaration = new BoundVariableDeclaration(node.Variable, node.LowerBound);
+            var variableExpression = new BoundVariableExpression(node.Variable);
+            var condition = new BoundBinaryExpression(
+                variableExpression, 
+                BoundBinaryOperator.Bind(SyntaxKind.LessOrEqualsToken, typeof(int), typeof(int)), 
+                node.UpperBound
+            );
+            var increment = new BoundExpressionStatement(
+                new BoundAssignmentExpression(node.Variable,
+                    new BoundBinaryExpression(
+                        variableExpression, 
+                        BoundBinaryOperator.Bind(SyntaxKind.PlusToken, typeof(int), typeof(int)),
+                        new BoundLiteralExpression(1)
+                    )
+                )
+            );
+            var whileBody = new BoundBlockStatement(ImmutableArray.Create(node.Body, increment));
+            var whileStatement = new BoundWhileStatement(condition, whileBody);
+            var result = new BoundBlockStatement(ImmutableArray.Create<BoundStatement>(variableDeclaration, whileStatement));
+
+            return RewriteStatement(result);
         }
     }
 }
