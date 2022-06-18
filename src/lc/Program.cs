fa9e98d19c10fd765ced7fe1a2721ed2bbfeeec7
Sun Zizhuo <211530w@student.hci.edu.sg>
2022-06-18 17:42:14 +0800

    Added compiler project and sample project

---
+++ src/lc/Program.cs
@@ -0,0 +1,42 @@
+﻿using Language.CodeAnalysis;
+using Language.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Language
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.Error.WriteLine("usage lc <source-paths>");
+                return;
+            }
+
+            if(args.Length > 1)
+            {
+                Console.WriteLine("error: only one path is currently supported");
+            }
+
+            var path = args.Single();
+            var text = File.ReadAllText(path);
+            var syntaxTree = SyntaxTree.Parse(text);
+            var compilation = new Compilation(syntaxTree);
+            var result = compilation.Evaluate(new Dictionary<CodeAnalysis.Symbols.VariableSymbol, object>());
+
+            if (!result.Diagnostics.Any())
+            {
+                if (result.Value != null)
+                    Console.WriteLine(result.Value);
+            }
+            else
+            {
+                Console.Error.WriteDiagnostics(result.Diagnostics, syntaxTree);
+            }
+        }
+    }
+}
